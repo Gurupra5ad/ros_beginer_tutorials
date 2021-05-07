3a3833de5bf4c15f264503dfd1c0f3e55cc46cff
Guru <guruprasad2511@gmail.com>
2021-05-07 12:16:39 +0530

    upadted iot publish

--- src/Iot_sensor_msg/iot_msg_publish.py
+++ src/Iot_sensor_msg/iot_msg_publish.py
@@ -18,9 +18,6 @@ def talker():
         pub.publish(iot_sensor)
         rate.sleep()
         i=i+1
-    
+
 if __name__ == "__main__":
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
+    talker()
